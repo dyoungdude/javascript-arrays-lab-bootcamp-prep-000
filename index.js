c028da40da8ab0b7f18da73ff0766f46941ae1f3
Darren Young <dlyoung5@asu.edu>
2018-07-17 04:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
-function destructivelyAppendKitten(kittens, name){
-  var name = "Ralph"
-  kittens.push(name);
+function destructivelyAppendKitten(){
+  kittens.push("Ralph");
   return kittens;
 }
 
-function destructivelyPrependKitten(kittens, name){
-  var name = "Bob"
-  kittens.unshift(name);
+function destructivelyPrependKitten(){
+  kittens.unshift('Bob');
   return kittens;
 }
 

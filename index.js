5276a394124b4b636f7c2bc240e00a7e18cb039b
Darren Young <dlyoung5@asu.edu>
2018-07-17 04:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyPrependKitten(){
 function destructivelyRemoveLastKitten(){
   return kittens.pop();
 }
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
